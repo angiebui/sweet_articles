4b92ac3ae6d17fec7f296ef5a6d9dd6fbba1a8c0
Apprentice <apprentice@devbootcamp.com>
2013-05-21 11:10:57 -0700

    Delete tags and category columns from articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130521170608) do
+ActiveRecord::Schema.define(:version => 20130521180832) do
 
   create_table "articles", :force => true do |t|
     t.string "title"
     t.text   "description"
     t.string "url"
-    t.string "category"
-    t.string "tags"
   end
 
   create_table "categories", :force => true do |t|
